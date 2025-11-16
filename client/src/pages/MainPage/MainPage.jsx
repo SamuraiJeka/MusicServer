bec3b6a7bab777e8feb6990e143a3f9e890ffe50
SamuraiJeka <offissit123321@gmail.com>
2025-11-16 22:06:43 +0300

    add song list widget

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,18 @@
 import styles from "./MainPage.module.scss"
+
 import AlbumList from "../../widgets/albumList/albumList";
 import ArtistList from "../../widgets/ArtistList/ArtistList"
+import SongList from "../../widgets/SongList/SongList";
 
 
 const MainPage = () => {
     return (
         <div className={styles.wrapper}>
-            <AlbumList/>
-            <ArtistList/>
+            <div className={styles.main_container}>
+                <SongList/>
+                <AlbumList/>
+                <ArtistList/>
+            </div>
         </div>
          
     )
